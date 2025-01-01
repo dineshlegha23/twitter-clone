10b996dd894002e017377fe6cff6d26cc66d1ee9
Dinesh <dineshlegha23@gmail.com>
2025-01-01 10:32:25 +0530

    implement user profile retrieval and follow/unfollow functionality

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
-import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
