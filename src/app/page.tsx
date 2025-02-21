515b31c031aafa14b2625cd487c2a42a0e6857a7
Dinesh <dineshlegha23@gmail.com>
2025-02-21 15:06:20 +0530

    added Image component and configure image handling with ImageKit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import Image from "@/components/Image";
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="">
+      <Image path="general/post.jpeg" w={600} h={600} alt="test post" />
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
